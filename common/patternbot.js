b8468a6fda7d0a387e3c9121e47b5d2fda957822
mart1003 <scottmartel@Scotts-MacBook-Pro.local>
2018-03-20 13:08:48 -0400

    Fix Indentation in html

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/scottmartel/Desktop/Graphic Design Second Year/Semester 2/Web Dev 4/geohub
- * @version 1521565570083
+ * @version 1521565713132
  */
-const patternManifest_1521565570083 = {
+const patternManifest_1521565713132 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -846,5 +846,5 @@ const patternManifest_1521565570083 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521565570083);
+patternBotIncludes(patternManifest_1521565713132);
 }());
